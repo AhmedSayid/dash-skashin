bc6739e6c1511642e0a604347c1d758fd6849925
Ahmed sayed <86841870+AhmedSayid@users.noreply.github.com>
2023-09-20 02:43:53 +0300

    dash-design-done

---
+++ src/App.jsx
@@ -0,0 +1,187 @@
+import './index.css'
+function App() {
+  return (
+    <>
+      <div className="container mt-5 p-2 w-100">
+        <div className='d-flex justify-content-between'>
+          <div className='text-primary fw-semibold'>User Management</div>
+          <div className='btn btn-outline-primary rounded-5 px-4 fw-semibold'> add user + </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#BCE790", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#E5B5EE", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#8DA1E4", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#E5B5EE", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#8DA1E4", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#E5B5EE", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#BCE790", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#E5B5EE", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#BCE790", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}          <div className='col-12 col-md-3 col-sm-6'>
+            <div style={{ height: '100px', boxShadow: ' rgba(149, 157, 165, 0.2) 0px 8px 24px' }} className='rounded-3 bg-light p-2 d-flex align-items-center mb-3'>
+              <div className='d-flex justify-content-around w-100'>
+                <div className='rounded-circle d-flex justify-content-center align-items-center text-center fs-4 text-light fw-bold' style={{ backgroundColor: "#E5B5EE", height: '60px', width: '60px' }} > AS </div>
+                <div className='d-flex flex-column justify-content-start'>
+                  <div className='fw-semibold '>Ahmed Sayed</div>
+                  <div className='text-secondary ' style={{ fontWeight: '400' }}>@akera</div>
+                  <div className='fw-bold'>CEO</div>
+                </div>
+                <div className='text-secondary'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                </svg></div>
+              </div>
+            </div>
+
+          </div>
+          {/*  */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
